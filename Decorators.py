fbb2517e6da554d39d6b0cde1e3120f60f458ea6
Przemysław Szewczak <szewczakprzemyslaw1@gmail.com>
2021-09-19 14:36:43 +0200

    Added database_exist decorator - Ver 1.1

--- Decorators.py
+++ Decorators.py
@@ -1,9 +1,12 @@
 import time
 import functools
-def function_timer(_func=None,*, mode=True):
+import psycopg2
+
+
+def function_timer(_func=None, *, mode=True):
     def timer(func):
         @functools.wraps(func)
-        def wrapper(*args,**kwargs):
+        def wrapper(*args, **kwargs):
             if mode == False:
                 value = func(*args, **kwargs)
                 return value
@@ -13,10 +16,39 @@ def function_timer(_func=None,*, mode=True):
             times = end - start
             print(f"Function {func.__name__!r} takes {times:.7f} secs")
             return value
+
         return wrapper
+
     if _func == None:
         return timer
     else:
         return timer(_func)
-def database_exist(_func=None,*,mode=True):
+
+
+def database_exist(_func=None, *, password=None):
+    def inner_func(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            if password is None:
+                val = func(*args, **kwargs)
+                return val
+            else:
+                with psycopg2.connect(host="localhost", user='postgres', password=password, port=5432) as conn:
+                    with conn.cursor() as curs:
+                        curs.execute("SELECT datname FROM pg_catalog.pg_database WHERE datname='finanse';")
+                        dblist = curs.fetchall()
+                        if dblist != [] and dblist[0][0] == 'finanse':
+                            print("Everything OK")
+                        else:
+                            print("Database does not exists! Create database 'finanse' using your psql console!")
+                            return False
+                val = func(*args, **kwargs)
+                return val
+
+        return wrapper
+
+    if _func is None:
+        return inner_func
+    else:
+        return inner_func(_func)
     pass
